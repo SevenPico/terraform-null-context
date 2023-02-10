bf10958dff6577dbc13c45980f8ffaa9ff34ad87
Dave Raring <d@7pi.co>
2023-02-10 17:00:27 -0600

    no message

--- examples/legacy/legacy.tf
+++ examples/legacy/legacy.tf
@@ -21,7 +21,7 @@
 ## ----------------------------------------------------------------------------
 
 module "legacy_label" {
-  source  = "registry.terraform.io/cloudposse/label/null"
+  source  = "cloudposse/label/null"
   version = "0.25.0" # requires Terraform >= 0.13.0
   context = module.context.legacy
   name    = "hello"
